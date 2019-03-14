b6d81ed269b9f5dcf5f036e952aaa26ee4c3efe5
Eszter <ekov@protonmail.ch>
2019-03-14 20:57:21 +0000

    core(theme): create theme & package config

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const theme = {
+  title: 'North',
+  baseFontSize: '17px',
+  baseLineHeight: 1.58,
+  blockMarginBottom: 1,
+  bodyColor: '#222',
+  headerFontFamily: 'sans-serif',
+  bodyFontFamily: 'sans-serif',
+};
+
+export default theme;
