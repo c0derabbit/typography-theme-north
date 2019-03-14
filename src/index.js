8169117ee4b0bbbf918c9170122259206e6a985a
Eszter <ekov@protonmail.ch>
2019-03-14 21:07:12 +0000

    fix(fonts): put fonts in an array

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const theme = {
   baseLineHeight: 1.58,
   blockMarginBottom: 1,
   bodyColor: '#222',
-  headerFontFamily: 'sans-serif',
-  bodyFontFamily: 'sans-serif',
+  headerFontFamily: ['sans-serif'],
+  bodyFontFamily: ['sans-serif'],
 };
 
 export default theme;
