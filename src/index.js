5ea65fe68811ecf27f9d39f189e5862221e33bf7
Eszter <ekov@protonmail.ch>
2019-03-15 21:26:30 +0000

    core(theme): add colour etc overrides

--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
+const systemFonts = [
+  '-apple-system',
+  'BlinkMacSystemFont',
+  'Segoe UI',
+  'Roboto',
+  'Oxygen',
+  'Ubuntu',
+  'Cantarell',
+  'Fira Sans',
+  'Droid Sans',
+  'Helvetica Neue',
+  'sans-serif',
+];
+
+const textColor = '#222';
+const accent = '#ff3322';
+
 const theme = {
   title: 'North',
   baseFontSize: '17px',
   baseLineHeight: 1.58,
+  headerLineHeight: 1.2,
+  scaleRatio: 2,
+  googleFonts: [],
+  headerFontFamily: systemFonts,
+  bodyFontFamily: systemFonts,
+  bodyColor: textColor,
+  headerColor: 'inherit',
+  bodyWeight: 400,
+  boldWeight: 600,
   blockMarginBottom: 1,
-  bodyColor: '#222',
-  headerFontFamily: ['sans-serif'],
-  bodyFontFamily: ['sans-serif'],
+  includeNormalize: true,
+  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+    body: {
+      letterSpacing: '.2px',
+    },
+    'h1,h2,h3,h4,h5,h6': {
+      letterSpacing: '.1px',
+    },
+    a: {
+      color: accent,
+      textDecoration: 'none',
+      transition: 'color .2s ease',
+    },
+    'a:hover,a:active': {
+      color: textColor,
+    },
+  }),
 };
 
 export default theme;
